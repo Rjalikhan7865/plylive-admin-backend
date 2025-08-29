822cafa3c83c614570163cfa4e7a25b1f7c2da87
Rjalikhan7865 <149364848+Rjalikhan7865@users.noreply.github.com>
2025-08-29 18:50:25 +0500

    Update config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MongoDb_Connection_String: "mongodb+srv://anas12:anass@cluster0.glzmvnr.mongodb.net/AdiminDetails?retryWrites=true&w=majority",
+  PORT: 9000,
+  Firebase: {
+    serviceAccountPath: "/home/eaglelive/public_html/admin/backend/plylive-662d2-firebase-adminsdk-fbsvc-103ea40a57.json"
+  },
+  JWT_SECRET: "453604537338"
+};
