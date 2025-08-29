7921cf8f2b945daf510d36a7d39db97217c58847
Rjalikhan7865 <149364848+Rjalikhan7865@users.noreply.github.com>
2025-08-29 18:53:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
   Firebase: {
     serviceAccountPath: "/home/eaglelive/public_html/admin/backend/plylive-662d2-firebase-adminsdk-fbsvc-103ea40a57.json"
   },
-  JWT_SECRET: "453604537338"
+  JWT_SECRET: "8c1d8e4f7e3a45a1b91d0a7c6f5b4e39f8c2a1d7e4b6c9a2f3d5b7c8e1f9a0b3"
 };
+
