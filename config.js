6abad17d861ed5ffe2173253b856f5477e6ae7d6
Rjalikhan7865 <149364848+Rjalikhan7865@users.noreply.github.com>
2025-08-30 13:59:52 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,32 @@
 module.exports = {
-  MongoDb_Connection_String: "mongodb+srv://anas12:anass@cluster0.glzmvnr.mongodb.net/AdiminDetails?retryWrites=true&w=majority",
+  //Port
   PORT: 9000,
+
+  //Project Name
+  projectName : "Admin-backend",
+
+  //Gmail credentials for send email
+  EMAIL: "$resend_email",
+
+  //Secret key for jwt
+  JWT_SECRET: "8c1d8e4f7e3a45a1b91d0a7c6f5b4e39f8c2a1d7e4b6c9a2f3d5b7c8e1f9a0b3",
+
+  //Secret key for API
+  secretKey: "8c1d8e4f7e3a45a1b91d0a7c6f5b4e39f8c2a1d7e4b6c9a2f3d5b7c8e1f9a0b3",
+
+  //baseURL
+  baseURL: "https://backend1.foydalive.com/",
+
+  //agency path
+  AGENCY_PATH: "https://backend3.foydalive.com/",
+
+  //host path
+  HOST_PATH: "https://backend2.foydalive.com/",
+
   Firebase: {
     serviceAccountPath: "/home/eaglelive/public_html/admin/backend/plylive-662d2-firebase-adminsdk-fbsvc-103ea40a57.json"
   },
-  JWT_SECRET: "8c1d8e4f7e3a45a1b91d0a7c6f5b4e39f8c2a1d7e4b6c9a2f3d5b7c8e1f9a0b3"
-};
 
+  //Mongodb string
+  MongoDb_Connection_String: "mongodb+srv://anas12:anass@cluster0.glzmvnr.mongodb.net/AdiminDetails?retryWrites=true&w=majority"
+};
